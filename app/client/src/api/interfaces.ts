5b846f8c2eca4414d5c64b4bbc7bad178163d693
fanni <fmalevych@zoolatech.com>
2024-03-31 23:35:04 +0300

    added requests for client

---
+++ app/client/src/api/interfaces.ts
@@ -0,0 +1,11 @@
+export interface ITaskData {
+  name: string;
+  listId: number;
+  description: string;
+  dueDate: Date;
+  priority: string;
+}
+
+export interface IListData {
+  name: string;
+}
